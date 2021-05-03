2a0da49c689643c497734c6179f9a6c50b4f262e
eamon3481 <68339352+eamon3481@users.noreply.github.com>
2021-05-03 19:42:32 +0900

    210503

---
+++ programmers/level2/영어 끝말잇기/eamon_anwser.js
@@ -0,0 +1,42 @@
+function solution(n, words) {
+
+
+  let stack = new Set();
+  let curNum = 1;
+  let cnt = {1:1};
+  let i = 0;
+  for (const v of words) {
+    if (i === 0) stack.add(v);
+    else {
+      if (cnt[curNum]) cnt[curNum] += 1;
+      else cnt[curNum] = 1;
+      const str = words[i - 1];
+      console.log(str, cnt);
+      if (str.slice(str.length - 1, str.length) !== v.slice(0, 1) || stack.has(v))
+        return [curNum, cnt[curNum]];
+        
+      stack.add(v);
+    }
+    if(curNum === n){
+        curNum=1
+    }else{
+        curNum++
+    }
+    
+    i++;
+    if (i === words.length) return [0, 0];
+  }
+
+}
+
+console.log(solution(3, [
+  "tank",
+  "kick",
+  "know",
+  "wheel",
+  "land",
+  "dream",
+  "mother",
+  "robot",
+  "tank",
+]));
