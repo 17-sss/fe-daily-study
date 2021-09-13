a4a1a9d4a0122ed7f9102ba66fb078a0ab023685
KH-20170630OXUD\Administrator <eamon3481@naver.com>
2021-09-14 00:49:14 +0900

    2021-09-14

--- programmers/level2/수식최대화/eamon_anwser.js
+++ programmers/level2/수식최대화/eamon_anwser.js
@@ -1,43 +1,47 @@
 function solution(expression) {
-  var answer = 0;
-  const num = expression.split(/[\+\-\*]/gi);
-  const express = expression.replace(/[0-9]/gi, "").split("");
-  let sum = [];
+ var answer = 0;
+ const num = expression.split(/[\+\-\*]/gi);
+ const express = expression.replace(/[0-9]/gi, '').split('');
+ // 숫자와 표현식
 
-  const cases = [
-    ["+", "-", "*"],
-    ["+", "*", "-"],
-    ["*", "+", "-"],
-    ["*", "-", "+"],
-    ["-", "+", "*"],
-    ["-", "*", "+"],
-  ];
+ let sum = [];
 
-  const BE = (nums, exp, c, i = 0) => {
-    if (i === 3) {
-      sum.push(Math.abs(nums));
-      return;
-    }
-    const idx = exp.indexOf(c[i]);
-    if (idx !== -1) {
-      const arr = [
-        ...nums.slice(0, idx),
-        eval(nums[idx] + c[i] + nums[idx + 1]),
-        ...nums.slice(idx + 2, nums.length),
-      ];
-      exp.splice(idx, 1);
-      //const newExp = exp.filter((v, i) => i !== idx);
-      BE(arr, exp, c, i);
-    } else {
-      i++;
-      BE(nums, exp, c, i);
-    }
-  };
-  cases.map((c) => BE(num, [...express], c));
-  return sum;
+ const cases = [
+  ['+', '-', '*'],
+  ['+', '*', '-'],
+  ['*', '+', '-'],
+  ['*', '-', '+'],
+  ['-', '+', '*'],
+  ['-', '*', '+'],
+ ];
+ // 나올수있는 경우의수
+
+ // 재귀함수 시작
+ const recursion = (nums, exp, c, i = 0) => {
+  if (i === 3) {
+   sum.push(Math.abs(nums));
+   return;
+  }
+
+  const idx = exp.indexOf(c[i]);
+  if (idx !== -1) {
+   const arr = [
+    ...nums.slice(0, idx),
+    eval(nums[idx] + c[i] + nums[idx + 1]),
+    ...nums.slice(idx + 2, nums.length),
+   ];
+   const newExp = exp.filter((v, i) => i !== idx);
+   recursion(arr, newExp, c, i);
+  } else {
+   i++;
+   recursion(nums, exp, c, i);
+  }
+ };
+ cases.map((c) => BE(num, express, c));
+ return Math.max.apply(null, sum);
 }
 
-const expression = "100-200*300-500+20";
+const expression = '100-200*300-500+20';
 
 console.log(solution(expression));
 // 빽 트레킹을 쓰려했으나 빽트레킹이 아닌 리컬션만 쓴거같다.
